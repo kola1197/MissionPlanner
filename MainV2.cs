4bf30b8427cfaede89206004d04f3884f5cc9bd1
mustafa-irina <mkir2505@gmail.com>
2020-10-14 06:37:03 +0300

    top panel MainButton

--- MainV2.cs
+++ MainV2.cs
@@ -1531,7 +1531,8 @@ namespace MissionPlanner
             FlightPlanner.mainMenuWidget1.RulerButton.Click += new EventHandler(rulerButtonsClick);
             engineController = new EngineController();
             timer1.Start();
-
+            FlightPlanner.mainMenuWidget1.Parent = FlightPlanner.MainMap;
+            FlightPlanner.mainMenuWidget1.MapChoiseButton.Parent = FlightPlanner.MainMap;
             FlightPlanner.wpMenu1.Parent = FlightPlanner.MainMap;
             /*FlightPlanner.wpMenu1.panel1.Parent = FlightPlanner.wpMenu1;
             FlightPlanner.wpMenu1.panel2.Parent = FlightPlanner.wpMenu1;
