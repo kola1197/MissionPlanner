432bab382a3998bf7f160610a05454c6c3706f89
NIKOLAY <kola-1197@mail.ru>
2020-10-14 04:40:17 +0300

    files

---
+++ Controls/NewControls/RulerControl.Designer.cs
@@ -0,0 +1,64 @@
+﻿namespace MissionPlanner.Controls.NewControls
+{
+    partial class RulerControl
+    {
+        /// <summary> 
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором компонентов
+
+        /// <summary> 
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(200, 24);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.WaitOnLoad = true;
+            // 
+            // RulerControl
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Transparent;
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "RulerControl";
+            this.Size = new System.Drawing.Size(200, 24);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        public System.Windows.Forms.Timer timer1;
+    }
+}
---
+++ Controls/NewControls/RulerControl.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MissionPlanner.Controls.NewControls
+{
+    public partial class RulerControl : UserControl
+    {
+        public RulerControl()
+        {
+            InitializeComponent();
+            this.DoubleBuffered = true;
+            recalculate(0,0);
+        }
+
+        Bitmap myBitmap;
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            
+        }
+
+        public void recalculate(int a, int b) 
+        {
+            int width = this.Size.Width;
+            int height = this.Size.Height;
+            myBitmap = new Bitmap(width, height);
+            //base.OnPaint(e);
+            Graphics g;
+            g = Graphics.FromImage(myBitmap);
+            Pen myPen = new Pen(Color.White);
+            myPen.Width = 4;
+
+            g.DrawLine(myPen, 1, height / 2, width - 1, height / 2);
+            g.DrawLine(myPen, 1, 0, 1, height);
+            g.DrawLine(myPen, width-1, 0, width - 1, height);
+
+
+            Pen p = new Pen(Color.Green);
+            p.Width = 3;
+
+            pictureBox1.Image = myBitmap;
+        }
+    }
+}
