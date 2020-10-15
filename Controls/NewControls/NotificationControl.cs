b1223d1fcfe07fbbbfdd6241d86256d9f038d272
NIKOLAY <kola-1197@mail.ru>
2020-10-15 03:21:40 +0300

    merge fix

--- Controls/NewControls/NotificationControl.cs
+++ Controls/NewControls/NotificationControl.cs
@@ -40,10 +40,10 @@ namespace MissionPlanner.Controls.NewControls
         {
             string currentFlightTime = "";
             string startFlightTime = "";
-            Orlan.AircraftConnectionInfo info;
+            AircraftConnectionInfo info;
             if (MainV2.comPort.MAV.cs.connected && MainV2.CurrentAircraftNum != null)
             {
-                if (MainV2._aircraftInfo.TryGetValue(MainV2.CurrentAircraftNum, out info))
+                if (MainV2.AircraftInfo.TryGetValue(MainV2.CurrentAircraftNum, out info))
                 {
                     DateTime now = DateTime.Now;
                     DateTime diff = new DateTime(0);
