f9525cb6e2454d4472b2bebf3264cac3b5436dba
Gvalhca <kir.diatloff@gmail.com>
2020-10-18 20:18:22 +0300

    engineControlForm move to MainV2.cs

--- Controls/NewControls/StatusControlPanel.cs
+++ Controls/NewControls/StatusControlPanel.cs
@@ -17,7 +17,7 @@ namespace MissionPlanner.Controls
     public partial class StatusControlPanel : UserControl
     {
         private readonly Point slidingScaleIndent;
-        private EngineControlForm engineControlForm;
+
         private Dictionary<ToolStripItem, SensorUserControl> sensors =
             new Dictionary<ToolStripItem, SensorUserControl>();
 
@@ -271,13 +271,7 @@ namespace MissionPlanner.Controls
 
         private void enginePanel_Click(object sender, EventArgs e)
         {
-            if (engineControlForm != null) 
-            {
-                engineControlForm.Close();
-            }
-            engineControlForm = new EngineControlForm();
-            engineControlForm.TopMost = true;
-            engineControlForm.Show();
+            MainV2.engineControlForm.Show();
         }
 
         private void speedPanel_Click(object sender, EventArgs e)
