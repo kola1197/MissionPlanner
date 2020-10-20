571622fca8694bd4231c4532584cbe43563ac994
NIKOLAY <kola-1197@mail.ru>
2020-10-20 05:38:06 +0300

    CoordinatsModeForm

--- Controls/NewControls/CoordinatsControl.cs
+++ Controls/NewControls/CoordinatsControl.cs
@@ -7,11 +7,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MissionPlanner.NewForms;
 
 namespace MissionPlanner.Controls.NewControls
 {
     public partial class CoordinatsControl : UserControl
     {
+        CoordinatsModeForm coordinatsModeForm;
         public CoordinatsControl()
         {
             InitializeComponent();
@@ -21,6 +23,17 @@ namespace MissionPlanner.Controls.NewControls
         private void label2_Click(object sender, EventArgs e)
         {
 
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            if (coordinatsModeForm != null)
+            {
+                coordinatsModeForm.Close();
+            }
+            coordinatsModeForm = new CoordinatsModeForm();
+            coordinatsModeForm.TopMost = true;
+            coordinatsModeForm.Show();
         }
     }
 }
