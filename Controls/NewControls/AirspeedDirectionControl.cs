943c61767858c020a7988cf61ad1fc74cc295a3f
NIKOLAY <kola-1197@mail.ru>
2020-10-21 05:08:52 +0300

    missing files

---
+++ Controls/NewControls/AirspeedDirectionControl.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MissionPlanner.Controls.NewControls
+{
+    public partial class AirspeedDirectionControl : UserControl
+    {
+        public AirspeedDirectionControl()
+        {
+            InitializeComponent();
+        }
+        Bitmap bitmap;
+        Bitmap bitmap1;
+        int counter = 0;
+        public void updateData() 
+        {
+            label1.Text = MainV2.comPort.MAV.cs.airspeed.ToString("0.0");
+            bitmap = new Bitmap(global::MissionPlanner.Properties.Resources.airspeedDirection);
+            if (MainV2.comPort.MAV.cs.connected)
+            {
+                bitmap1 = RotateImage(bitmap, MainV2.comPort.MAV.cs.wind_dir);
+            }
+            else 
+            {
+                bitmap1 = RotateImage(bitmap, 0);
+                //counter++;
+            }
+            this.BackgroundImage = bitmap1;
+
+        }
+
+        private Bitmap RotateImage(Bitmap bmp, float angle)
+        {
+            Bitmap rotatedImage = new Bitmap(bmp.Width, bmp.Height);
+            rotatedImage.SetResolution(bmp.HorizontalResolution, bmp.VerticalResolution);
+
+            using (Graphics g = Graphics.FromImage(rotatedImage))
+            {
+                // Set the rotation point to the center in the matrix
+                g.TranslateTransform(bmp.Width / 2, bmp.Height / 2);
+                // Rotate
+                g.RotateTransform(angle);
+                // Restore rotation point in the matrix
+                g.TranslateTransform(-bmp.Width / 2, -bmp.Height / 2);
+                // Draw the image on the bitmap
+                g.DrawImage(bmp, new Point(0, 0));
+            }
+
+            return rotatedImage;
+        }
+    }
+}
