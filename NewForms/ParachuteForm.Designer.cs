19ff4a829936296a005992003a6c6916058a34e4
mustafa-irina <mkir2505@gmail.com>
2020-10-28 19:32:41 +0300

    дизайн форм

--- NewForms/ParachuteForm.Designer.cs
+++ NewForms/ParachuteForm.Designer.cs
@@ -35,38 +35,48 @@
             // 
             // button1
             // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(196)))), ((int)(((byte)(196)))), ((int)(((byte)(196)))));
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.ForeColor = System.Drawing.Color.Black;
             this.button1.Location = new System.Drawing.Point(12, 12);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(152, 112);
             this.button1.TabIndex = 0;
             this.button1.Text = "Выпустить парашют";
-            this.button1.UseVisualStyleBackColor = true;
+            this.button1.UseVisualStyleBackColor = false;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
+            this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(196)))), ((int)(((byte)(196)))), ((int)(((byte)(196)))));
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.ForeColor = System.Drawing.Color.Black;
             this.button2.Location = new System.Drawing.Point(170, 12);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(152, 112);
             this.button2.TabIndex = 1;
             this.button2.Text = "Рули в нейтраль";
-            this.button2.UseVisualStyleBackColor = true;
+            this.button2.UseVisualStyleBackColor = false;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // button3
             // 
+            this.button3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(196)))), ((int)(((byte)(196)))), ((int)(((byte)(196)))));
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.ForeColor = System.Drawing.Color.Black;
             this.button3.Location = new System.Drawing.Point(328, 12);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(152, 112);
             this.button3.TabIndex = 2;
             this.button3.Text = "Отцеп открыть";
-            this.button3.UseVisualStyleBackColor = true;
+            this.button3.UseVisualStyleBackColor = false;
             this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // ParachuteForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(32)))), ((int)(((byte)(32)))), ((int)(((byte)(32)))));
             this.ClientSize = new System.Drawing.Size(491, 130);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
@@ -74,6 +84,7 @@
             this.Name = "ParachuteForm";
             this.Text = "Панель работы с парашютом";
             this.ResumeLayout(false);
+
         }
 
         #endregion
