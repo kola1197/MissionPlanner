5b97cc0119e93c893d4324dd50c73ee1b51e0581
Gvalhca <kir.diatloff@gmail.com>
2021-03-22 07:46:15 +0300

    Min height 80 -> 40

--- NewForms/RouteAltForm.Designer.cs
+++ NewForms/RouteAltForm.Designer.cs
@@ -36,8 +36,8 @@ namespace MissionPlanner.NewForms
             // 
             // route_BUT
             // 
-            this.route_BUT.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(196)))), ((int)(((byte)(196)))), ((int)(((byte)(196)))));
-            this.route_BUT.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(196)))), ((int)(((byte)(196)))), ((int)(((byte)(196)))));
+            this.route_BUT.BackColor = System.Drawing.Color.FromArgb(((int) (((byte) (196)))), ((int) (((byte) (196)))), ((int) (((byte) (196)))));
+            this.route_BUT.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int) (((byte) (196)))), ((int) (((byte) (196)))), ((int) (((byte) (196)))));
             this.route_BUT.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.route_BUT.ForeColor = System.Drawing.Color.Black;
             this.route_BUT.Location = new System.Drawing.Point(2, 1);
@@ -52,24 +52,24 @@ namespace MissionPlanner.NewForms
             // 
             this.alt_SlidingScale.BackColor = System.Drawing.Color.White;
             this.alt_SlidingScale.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
-            this.alt_SlidingScale.CenterFont = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.alt_SlidingScale.CenterRectangleDisabledColor = System.Drawing.Color.FromArgb(((int)(((byte)(200)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
-            this.alt_SlidingScale.CenterRectangleEnabledColor = System.Drawing.Color.FromArgb(((int)(((byte)(200)))), ((int)(((byte)(30)))), ((int)(((byte)(144)))), ((int)(((byte)(255)))));
-            this.alt_SlidingScale.CenterValueColor = System.Drawing.Color.FromArgb(((int)(((byte)(240)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
-            this.alt_SlidingScale.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.alt_SlidingScale.CenterFont = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte) (204)));
+            this.alt_SlidingScale.CenterRectangleDisabledColor = System.Drawing.Color.FromArgb(((int) (((byte) (200)))), ((int) (((byte) (128)))), ((int) (((byte) (128)))), ((int) (((byte) (128)))));
+            this.alt_SlidingScale.CenterRectangleEnabledColor = System.Drawing.Color.FromArgb(((int) (((byte) (200)))), ((int) (((byte) (30)))), ((int) (((byte) (144)))), ((int) (((byte) (255)))));
+            this.alt_SlidingScale.CenterValueColor = System.Drawing.Color.FromArgb(((int) (((byte) (240)))), ((int) (((byte) (255)))), ((int) (((byte) (255)))));
+            this.alt_SlidingScale.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte) (204)));
             this.alt_SlidingScale.ForeColor = System.Drawing.Color.Black;
             this.alt_SlidingScale.LargeTicksCount = 10;
             this.alt_SlidingScale.LargeTicksLength = 20;
             this.alt_SlidingScale.Location = new System.Drawing.Point(2, 30);
             this.alt_SlidingScale.MaxValue = 5100D;
-            this.alt_SlidingScale.MinValue = 80D;
+            this.alt_SlidingScale.MinValue = 40D;
             this.alt_SlidingScale.Name = "alt_SlidingScale";
             this.alt_SlidingScale.Orientation = System.Windows.Forms.Orientation.Vertical;
             this.alt_SlidingScale.ScaleRange = 1000D;
             this.alt_SlidingScale.Size = new System.Drawing.Size(83, 237);
             this.alt_SlidingScale.SmallTickLength = 7;
             this.alt_SlidingScale.TabIndex = 1;
-            this.alt_SlidingScale.Value = 80D;
+            this.alt_SlidingScale.Value = 40D;
             this.alt_SlidingScale.ValueChanged += new System.EventHandler<MissionPlanner.ValueChangedEventArgs>(this.alt_SlidingScale_ValueChanged);
             this.alt_SlidingScale.MouseDown += new System.Windows.Forms.MouseEventHandler(this.alt_SlidingScale_MouseDown);
             this.alt_SlidingScale.MouseMove += new System.Windows.Forms.MouseEventHandler(this.alt_SlidingScale_MouseMove);
@@ -91,7 +91,6 @@ namespace MissionPlanner.NewForms
             this.Text = "RouteAltForm";
             this.Shown += new System.EventHandler(this.RouteAltForm_Shown);
             this.ResumeLayout(false);
-
         }
 
         private TB.Instruments.SlidingScale alt_SlidingScale;
